389503929555a7a9248844c763b3f3118bb5af5a
hannahjavs <hannahsiujadavji@gmail.com>
2017-12-11 15:36:11 +0000

    Creating dependency and Withdrawal Method

---
+++ BankAccount.php
@@ -0,0 +1,40 @@
+<?
+  // CREATING THE DEPENDENCY:
+  abstract class BankAccount{
+    // PROTECTED property because it will be blackboxed
+    // PROTECTED because don't want anything affecting the account
+    protected $Balance = 0;
+    public $APR;
+    public $SortCode;
+    public $FirstName;
+    public $LastName;
+
+    // Is an empty array so we can push information into it
+    // Can keep information about the acocunt in here e.g withdrawals/deposits/account locked or unlocked:
+    public $Audit = array(); // sub-arrays below will be pushed into this main Audit array
+
+    // The account is LOCKED by default and protected:
+    protected $Locked = false;
+
+
+    //////////METHODS//////////
+
+    // Need to make sure there is a date and time for each transaction for fraud prevention!
+    public function WithDraw( $amount ){
+      $trainsDate = new DateTime(); // This is a date & time object
+      // if the back account is locked we do not want to allow the transaction to work
+      // if the locked property is false then the account is not locked
+      if( $this->Locked === false ){
+        // whatever the BALANCE is MINUS the AMOUNT and reasign the value back to the balance PROPERTY
+        $this->Balance -= $amount;
+        // this is a sub-array that will be pushed inside the original Audit array (at the top of the code)
+        // Can see the amount from the account that was successfully withdrawn from the account, then can see the new balance after the transaction has occured, then see a date and time and format it into a string using the 'c':
+        array_push($this -> Audit, array("WITHDRAW ACCEPTED",$amount, $this->Balance, $transDate->format('c') ) );
+      } else {
+        array_push( $this->Audit, array("WITHDRAW DENIED", $amount, $this->Balance, $transDate->format('c') ) );
+      }
+    }
+
+
+  }
+?>
